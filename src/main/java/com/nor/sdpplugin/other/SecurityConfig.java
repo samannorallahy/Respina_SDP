a2d19d2b04b0ec8d28168a49ad91cac572279efd
saman <samannorallahy@yahoo.com>
2025-08-13 11:22:30 +0330

    14040522

--- src/main/java/com/nor/sdpplugin/other/SecurityConfig.java
+++ src/main/java/com/nor/sdpplugin/other/SecurityConfig.java
@@ -1,24 +1,25 @@
-package com.nor.sdpplugin.other;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/secure-data").authenticated()
-                        .anyRequest().permitAll()
-                )
-                .httpBasic() // فعال‌سازی Basic Auth
-                .and()
-                .csrf().disable(); // برای راحتی تست، CSRF رو غیرفعال می‌کنیم
-
-        return http.build();
-    }
-}
+//package com.nor.sdpplugin.other;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.beans.Customizer;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/api/**").authenticated()
+//                        .anyRequest().permitAll()
+//                )
+//                .httpBasic(Customizer.withDefaults())
+//                .csrf(csrf -> csrf.disable());
+//
+//        return http.build();
+//    }
+//}
